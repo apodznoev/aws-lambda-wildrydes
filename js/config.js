01849b9d359ae3cfefa1afc9d0ee0fb45bac8524
apodznoev <a.podznoev@reply.de>
2019-11-14 11:15:21 +0100

    Added Cognito public credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_1hQEL28al', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '328h3rca4po9mrbta9m9r8fer4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
